23443554c96a4bf6aa6a31d20a61159b86542ed2
ArthurLuzz <50251309+ArthurLuzz@users.noreply.github.com>
2019-11-04 20:32:45 -0300

    Add files via upload

---
+++ brickBreaker/tela.js
@@ -0,0 +1,13 @@
+function telaInicial(){
+    background(139,0,139);
+    textSize(35);
+    fill(65,105,225)
+    text('Jogo aprimorado pelos programadores de auto nivel:', 35,300);
+  fill(25,25,112)
+  text('Arthur Vinicius,', 300,350);
+   text('Rafael Castro,', 300,400);
+   text('Carlos Eduardo. ', 300,450);
+   textSize(70);
+   fill(155,233,33) // cor
+   text("Brick Breaker", 200, 200)
+}
